3dc917df1b23733b630874a4d805770126cf6d5f
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-14 11:35:45 -0500

    screenshot added

---
+++ readme.md
@@ -0,0 +1,3 @@
+Explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+
--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 console.log("hello world")
 var arrayQs = [
     {
-        question: "1 + 1",
+        question: "What is 1 + 1?",
         answers: ["3", "2", "4", "5"],
         correct: "2"
     },
     {
-        question: "1 + 2",
+        question: "What is 1 + 2?",
         answers: ["3", "2", "4", "5"],
         correct: "3"
     },
     {
-        question: "1 + 3",
+        question: "What is 1 + 3?",
         answers: ["3", "2", "4", "5"],
         correct: "4"
     },
     {
-        question: "1 + 4",
+        question: "What is 1 + 4?",
         answers: ["3", "2", "4", "5"],
         correct: "5"
     },
     {
-        question: "1 + 5",
+        question: "What is 1 + 5?",
         answers: ["6", "2", "4", "5"],
         correct: "6"
     },
     {
-        question: "1 + 6",
+        question: "What is 1 + 6?",
         answers: ["7", "2", "4", "5"],
         correct: "7"
     },
     {
-        question: "1 + 7",
+        question: "What is 1 + 7?",
         answers: ["8", "2", "4", "5"],
         correct: "8"
     },
     {
-        question: "1 + 8",
+        question: "What is 1 + 8?",
         answers: ["9", "2", "4", "5"],
         correct: "9"
     },
     {
-        question: "1 + 9",
+        question: "What is 1 + 9?",
         answers: ["10", "2", "4", "5"],
         correct: "10"
     },
     {
-        question: "1 + 10",
+        question: "What is 1 + 10?",
         answers: ["11", "2", "4", "5"],
         correct: "11"
     }
@@ -59,7 +59,7 @@ var question = document.getElementsByClassName('question')[0]
 var answersShown = document.getElementsByClassName('answer')
 var restart = document.getElementById('restart')
 // insert question + answers into board
-
+// FIX: refactor:
 function makeQuestions() {
     for (var i = 0; i < arrayQs.length; i++) {
         // create div to hold question
@@ -100,6 +100,7 @@ function makeQuestions() {
 var scoreBoard = document.querySelector('#score')
 // test individual question response
 function runTest() {
+    // FIX: .correct selecting first on page, instead of current Element
     var rightAnswer = document.querySelector('.correct');        if (rightAnswer.checked) {
             console.log(this.parentElement);
             this.parentElement.classList.add('rightAnswer');this.parentElement.textContent = "";
